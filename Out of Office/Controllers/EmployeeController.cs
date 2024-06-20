899317d0f44312431fde8e8baff159bd40705027
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-06-20 22:53:50 +0200

    Employee controller added with one endpoint

---
+++ Out of Office/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Out_of_Office.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [SwaggerOperation(Summary = "Retrieves lists of all employees")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployeesAsync()
+        {
+            var employees = await _employeeService.GetAllEmployeesAsync();
+            if(employees == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employees);
+        }
+    }
+}
