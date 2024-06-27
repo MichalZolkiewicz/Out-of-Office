07816fc3a10e2b571c8e7ead3634c4b6796e8245
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-06-27 19:51:29 +0200

    Response wrapper created

---
+++ Out of Office/Wrapper/Response.cs
@@ -0,0 +1,38 @@
+﻿namespace Out_of_Office.Wrapper;
+
+public class Response<T> : Response
+{
+
+    public T Data { get; set; }
+    public bool Succeeded { get; set; }
+    public string Message { get; set; }
+    public IEnumerable<string> Errors { get; set; }
+    public Response()
+    {
+
+    }
+
+    public Response(T data)
+    {
+        Data = data;
+        Succeeded = true;
+    }
+}
+
+public class Response
+{
+    public bool Succeeded { get; set; }
+    public string Message { get; set; }
+
+    public Response()
+    {
+
+    }
+
+    public Response(bool succeded, string message)
+    {
+        Succeeded = succeded;
+        Message = message;
+    }
+}
+
