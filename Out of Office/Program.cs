7707545cf59d84e70f53412d432931e876032e86
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-06-29 02:06:59 +0200

    enabled annotation for swagger

--- Out of Office/Program.cs
+++ Out of Office/Program.cs
@@ -8,7 +8,10 @@ builder.Services.InstallServicesInAssembly(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+});
 
 
 
