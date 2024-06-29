ed4e66afa9990c7a15e44576189a38e7b8986ecb
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-06-29 02:07:19 +0200

    UserId changed to string

---
+++ Infrastructure/Migrations/20240628231553_UserIdPropertiesChangedToString.cs
@@ -0,0 +1,221 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserIdPropertiesChangedToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApprovalRequests_AspNetUsers_ApproverId1",
+                table: "ApprovalRequests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeaveRequests_AspNetUsers_UserId1",
+                table: "LeaveRequests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_AspNetUsers_ProjectManagerId1",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_ProjectManagerId1",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LeaveRequests_UserId1",
+                table: "LeaveRequests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ApprovalRequests_ApproverId1",
+                table: "ApprovalRequests");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectManagerId1",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "LeaveRequests");
+
+            migrationBuilder.DropColumn(
+                name: "ApproverId1",
+                table: "ApprovalRequests");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProjectManagerId",
+                table: "Projects",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "LeaveRequests",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApproverId",
+                table: "ApprovalRequests",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_ProjectManagerId",
+                table: "Projects",
+                column: "ProjectManagerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequests_UserId",
+                table: "LeaveRequests",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApprovalRequests_ApproverId",
+                table: "ApprovalRequests",
+                column: "ApproverId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApprovalRequests_AspNetUsers_ApproverId",
+                table: "ApprovalRequests",
+                column: "ApproverId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeaveRequests_AspNetUsers_UserId",
+                table: "LeaveRequests",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_AspNetUsers_ProjectManagerId",
+                table: "Projects",
+                column: "ProjectManagerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApprovalRequests_AspNetUsers_ApproverId",
+                table: "ApprovalRequests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeaveRequests_AspNetUsers_UserId",
+                table: "LeaveRequests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_AspNetUsers_ProjectManagerId",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_ProjectManagerId",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LeaveRequests_UserId",
+                table: "LeaveRequests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ApprovalRequests_ApproverId",
+                table: "ApprovalRequests");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectManagerId",
+                table: "Projects",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProjectManagerId1",
+                table: "Projects",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "LeaveRequests",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "LeaveRequests",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ApproverId",
+                table: "ApprovalRequests",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApproverId1",
+                table: "ApprovalRequests",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_ProjectManagerId1",
+                table: "Projects",
+                column: "ProjectManagerId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequests_UserId1",
+                table: "LeaveRequests",
+                column: "UserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApprovalRequests_ApproverId1",
+                table: "ApprovalRequests",
+                column: "ApproverId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApprovalRequests_AspNetUsers_ApproverId1",
+                table: "ApprovalRequests",
+                column: "ApproverId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeaveRequests_AspNetUsers_UserId1",
+                table: "LeaveRequests",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_AspNetUsers_ProjectManagerId1",
+                table: "Projects",
+                column: "ProjectManagerId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
