43cbbe2a7877b283341a547ea7711cf188cd0ac3
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-06-29 16:17:39 +0200

    swagger examples for leave request created

---
+++ Out of Office/SwaggerExamples/LeaveReqestExample.cs
@@ -0,0 +1,20 @@
+﻿using Application.Dto.LeaveRequests;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace Out_of_Office.SwaggerExamples;
+
+public class LeaveReqestExample : IExamplesProvider<CreateLeaveRequestDto>
+{
+    public CreateLeaveRequestDto GetExamples()
+    {
+        return new CreateLeaveRequestDto
+        {
+            UserId = "yourUserId",
+            AbsenceReason = "absenceReason",
+            StartDate = DateTime.Now,
+            EndDate = DateTime.Now,
+            Comment = "yourComment",
+            Status = "Awaiting Approval"
+        };
+    }
+}
