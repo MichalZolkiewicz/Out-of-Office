3478dfa5c35b5dcedc24197d6019192f4fd4b4ec
MichalZolkiewicz <zolkiewicz.michal@gmail.com>
2024-06-30 20:20:35 +0200

    example for approval request

---
+++ Out of Office/SwaggerExamples/ApprovalRequestExample.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dto.ApprovalRequests;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace Out_of_Office.SwaggerExamples;
+
+public class ApprovalRequestExample : IExamplesProvider<CreateApprovalRequestDto>
+{
+    public CreateApprovalRequestDto GetExamples()
+    {
+        return new CreateApprovalRequestDto
+        {
+            LeaveRequestId = 0,
+            Status = "Created",
+            Comment = "insertComment"
+        };
+    }
+}
